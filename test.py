464e59909217833eb5bceecb2c56ff1c732a66e8
khoatruong1799 <khoa.truong1799@gmail.com>
2020-12-26 20:23:08 +0700

    add test face recognition

--- test.py
+++ test.py
@@ -1,4 +1,26 @@
-seq = ('name', 'age', 'sex')
+import numpy as np
+from sklearn.svm import SVC
 
-dict = dict.fromkeys(seq, (10,12,12))
-print(dict)
+N = 1000  # Number of samples
+
+# Create synthetic dataset
+X1 = np.random.normal(loc=0, scale=1, size=(N, 2))
+Y1 = 0 * np.ones(shape=(1000,))  # LABEL = 0 
+print(type(X1[-1][-1]))
+
+X2 = np.random.normal(loc=[-5, 5], scale=1, size=(N, 2))
+Y2 = 1 * np.ones(shape=(1000,))  # LABEL = 1
+
+X3 = np.random.normal(loc=[5, -5], scale=1, size=(N, 2))
+Y3 = 2 * np.ones(shape=(1000,))  # LABEL = 2
+
+# Create stacked dataset
+X = np.vstack((X1, X2, X3))
+Y = np.hstack((Y1, Y2, Y3))
+print(type(X))
+
+# TRAIN SVM LEARNING ALGORITHM
+clf = SVC(kernel='linear')
+clf = clf.fit(X, Y)
+
+
