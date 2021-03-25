72568e03914a7aa15ae12b67bfe97f23201c1725
Khoa Truong <khoa.truong1799@gmail.com>
2021-03-25 22:03:47 +0700

    m

--- gui/gui.py
+++ gui/gui.py
@@ -70,6 +70,11 @@ class GUI(QtWidgets.QMainWindow):
 
         self.list_shape_face = [self.up_face, self.down_face, self.left_face, self.right_face, self.front_face]
 
+        img = cv2.imread('/home/thesis/Documents/E-Healthcare-System-Server/Manipulate_Data/Original_Face/train/1/IMG_3415.jpg')
+        img = cv2.resize(img, (430,400))
+        qp_image = self.__Convert_To_Display(img)
+        self.image_patient.setPixmap(qp_image)
+
     def __OnConfirmInforListenning(self):
         # opt = 1: confirm user
         if glo_va.STATE == glo_va.STATE_CONFIRM_PATIENT:
