b176195daa03c55c636095c322937abca2cbb4d9
khoatruong1799 <khoa.truong1799@gmail.com>
2021-04-18 13:15:21 +0700

    add torch

---
+++ model_engine/pytorch/change_resolution_onnx.py
@@ -0,0 +1,23 @@
+import onnx
+import math
+
+input_size =(320,240)
+model = onnx.load_model("/home/tuan/CenterFace.pytorch/centerface.onnx")
+d = model.graph.input[0].type.tensor_type.shape.dim
+# print("In", d)
+# for output in model.graph.output:
+#     d = output.type.tensor_type.shape.dim
+#     print("Out:", d)
+rate = (int(math.ceil(input_size[0]/d[2].dim_value)),int(math.ceil(input_size[1]/d[3].dim_value)))
+print("rate",rate)
+d[0].dim_value = 1
+d[2].dim_value *= rate[1]
+d[3].dim_value *= rate[0]
+for output in model.graph.output:
+    d = output.type.tensor_type.shape.dim
+    print(d)
+    d[0].dim_value = 1
+    d[2].dim_value  *= rate[1]
+    d[3].dim_value  *= rate[0]
+
+onnx.save_model(model,"centerface_320_240.onnx" )
